7c5e5c333ad907a940696da3184dfac059268c04
EmsWong <130682939+EmsWong@users.noreply.github.com>
2023-06-05 14:18:17 -0400

    Colour Hovering
    When hovering on an option on main menu, colour changes to make it stand out more

--- cptemilyhangman.java
+++ cptemilyhangman.java
@@ -1,6 +1,6 @@
 //Hangman
 //Emily Wong
-//v#1
+//v#2
 import arc.*;
 import java.awt.image.BufferedImage;
 import java.awt.Color;
@@ -9,25 +9,65 @@ import java.awt.Font;
 public class cptemilyhangman{
 	public static void main(String[] args){
 		Console con = new Console("Hangman", 1280, 720);
+		//Variables
+		boolean blnRepeat = true;
+		int intMouseXLoc = 0;
+		int intMouseYLoc = 0;
+		int intClick = 0;
+		
 		//Showing main screen when program is opened
 		mainmenubg(con);
+		
+		//Hovering on options
+		while(blnRepeat == true){
+			intMouseXLoc = con.currentMouseX();
+			intMouseYLoc = con.currentMouseY();
+			intClick = con.currentMouseButton();
+			if(intMouseXLoc >= 420 && intMouseXLoc <= 845 && intMouseYLoc >= 240 && intMouseYLoc <= 320){
+				con.setDrawColor(Color.RED);
+				con.drawString("◆ Play Game", 440, 225);
+				con.repaint();
+			}else if(intMouseXLoc >= 420 && intMouseXLoc <= 845 && intMouseYLoc >= 340 && intMouseYLoc <= 420){
+				con.setDrawColor(Color.RED);
+				con.drawString("◆ Help", 440, 325);
+				con.repaint();
+			}else if(intMouseXLoc >= 420 && intMouseXLoc <= 845 && intMouseYLoc >= 440 && intMouseYLoc <= 520){
+				con.setDrawColor(Color.RED);
+				con.drawString("◆ Leaderboard", 440, 425);
+				con.repaint();
+			}else if(intMouseXLoc >= 420 && intMouseXLoc <= 845 && intMouseYLoc >= 540 && intMouseYLoc <= 620){
+				con.setDrawColor(Color.RED);
+				con.drawString("◆ Add Theme", 440, 525);
+				con.repaint();
+			}else if(intMouseXLoc >= 0 && intMouseXLoc <= 150 && intMouseYLoc >= 630){
+				con.setDrawColor(Color.RED);
+				con.drawString("Quit", 10, 615);
+				con.repaint();
+			}else{
+				con.setDrawColor(Color.WHITE);
+				con.drawString("◆ Play Game", 440, 225);
+				con.drawString("◆ Help", 440, 325);
+				con.drawString("◆ Leaderboard", 440, 425);
+				con.drawString("◆ Add Theme", 440, 525);
+				con.drawString("Quit", 10, 615);
+				con.repaint();
+			}
+		}
 	}
 	//Main Menu Screen
 	public static void mainmenubg(Console con){
 		BufferedImage imgMenuBG = con.loadImage("mainmenubg.png");
 		con.drawImage(imgMenuBG, 0, 0);
-		BufferedImage imgBulletPoint = con.loadImage("mainmenubg.png");
-		con.drawImage(imgBulletPoint, 0, 0);
 		con.setDrawColor(Color.WHITE);
 		Font fntCabin = con.loadFont("CabinSketch-Bold.ttf", 120);
 		con.setDrawFont(fntCabin);
 		con.drawString("Hangman", 375, 35);
 		Font fntSans = con.loadFont("SourceSansPro-Black.ttf", 60);
 		con.setDrawFont(fntSans);
-		con.drawString("Play Game", 475, 225);
-		con.drawString("Help", 475, 325);
-		con.drawString("Leaderboard", 475, 425);
-		con.drawString("Add Theme", 475, 525);
+		con.drawString("◆ Play Game", 440, 225);
+		con.drawString("◆ Help", 440, 325);
+		con.drawString("◆ Leaderboard", 440, 425);
+		con.drawString("◆ Add Theme", 440, 525);
 		con.drawString("Quit", 10, 615);
 		con.repaint();
 	}
