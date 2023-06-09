81cf14ff723d3f52e250166f50b1469433a9e9a0
EmsWong <130682939+EmsWong@users.noreply.github.com>
2023-06-09 09:30:37 -0400

    Created a bubble sort and way to access text files
    Forgot to push it last night

--- cptemilyhangman.java
+++ cptemilyhangman.java
@@ -11,9 +11,9 @@ public class cptemilyhangman{
 		Console con = new Console("Hangman", 1280, 720);
 		//Theme Variables
 		String strTheme;
-		TextInputFile txtSports = new TextInputFile("sports.txt");
-		TextInputFile txtVegetables = new TextInputFile("vegetables.txt");
-		TextInputFile txtFruits = new TextInputFile("fruits.txt");
+		String strThemeTxt;
+		String strWords[][];
+		int intWords;
 		//Location Variables
 		boolean blnRepeat = true;
 		int intMouseXLoc = 0;
@@ -40,8 +40,14 @@ public class cptemilyhangman{
 			if(intMouseXLoc >= 420 && intMouseXLoc <= 845 && intMouseYLoc >= 240 && intMouseYLoc <= 320 && intClick == 1){
 				themes(con);
 				blnRepeat = false;
-				con.println("Enter the theme name including the .txt");
+				con.println("Enter the theme name:");
 				strTheme = con.readLine();
+				strThemeTxt = strTheme+".txt";
+				intWords = countWords(strThemeTxt);
+				strWords = loadThemeWords(intWords, strThemeTxt);
+				strWords = sortWords(strWords, intWords);
+				printWords(strWords, intWords, con);
+				
 			//Hover on Play Game
 			}else if(intMouseXLoc >= 420 && intMouseXLoc <= 845 && intMouseYLoc >= 240 && intMouseYLoc <= 320){
 				con.setDrawColor(Color.RED);
@@ -107,17 +113,84 @@ public class cptemilyhangman{
 		con.drawString("Quit", 10, 615);
 		con.repaint();
 	}
+	//Header
+	public static void header(Console con, String strName, String strTheme){
+		
+	}
 	//Themes Screen
 	public static void themes(Console con){
+		Font fntSans = con.loadFont("SourceSansPro-Black.ttf", 30);
+		con.setTextFont(fntSans);
 		TextInputFile txtThemes = new TextInputFile("themes.txt");
 		String strThemes;
 		con.setDrawColor(Color.BLACK);
 		con.fillRect(0, 0, 1280, 720);
 		con.repaint();
-		con.println("What theme would you like to play?");
+		con.println("\n\nWhat theme would you like to play?");
 		while(txtThemes.eof()==false){
 			strThemes = txtThemes.readLine();
 			con.println(strThemes);
 		}
 	}
+	//Counting Lines in File
+	public static int countWords(String strFileName){
+		TextInputFile txtWords = new TextInputFile(strFileName);
+		int intLines = 0;
+		String strData;
+		while(txtWords.eof() == false){
+			strData = txtWords.readLine();
+			intLines++;
+		}
+		txtWords.close();
+		return intLines;
+	}
+	//Assigning Random Number to Word
+	public static String[][] loadThemeWords(int intCount, String strFileName){
+		String strWords[][] = new String[intCount][2];
+		TextInputFile txtWords = new TextInputFile(strFileName);
+		int intRow;
+		int intRand;
+		for(intRow = 0; intRow < intCount; intRow++){
+			strWords[intRow][0] = txtWords.readLine();
+			intRand = (int)(Math.random()*100+1);
+			strWords[intRow][1] = Integer.toString(intRand) ;
+		}
+		txtWords.close();
+		return strWords;
+	}
+	//Printing Text File from Array
+	public static void printWords(String strWords[][], int intCount, Console con){
+		int intRow;
+		for(intRow = 0; intRow < intCount; intRow++){
+			con.print((strWords[intRow][0] + ":                                    ").substring(0,15));
+			con.print((strWords[intRow][1] + "                                     ").substring(0,20));
+			con.println("");
+		}
+	}
+	//Bubble Sort
+	public static String[][] sortWords(String strWords[][], int intWords){
+		int intBelow;
+		int intCurrent;
+		int intCounter;
+		int intCounter2;
+		String strTemp;
+		for(intCounter2 = 0; intCounter2 < intWords-1; intCounter2++){
+			for(intCounter = 0; intCounter < intWords-intCounter2-1; intCounter++){
+				intBelow = Integer.parseInt(strWords[intCounter+1][1]);
+				intCurrent = Integer.parseInt(strWords[intCounter][1]);
+				if(intBelow > intCurrent){
+					strTemp = strWords[intCounter+1][0];
+					strWords[intCounter+1][0] = strWords[intCounter][0];
+					strWords[intCounter][0] = strTemp;
+					strTemp = strWords[intCounter+1][1];
+					strWords[intCounter+1][1] = strWords[intCounter][1];
+					strWords[intCounter][1] = strTemp;
+				}
+			}
+		}
+		
+		return strWords;
+	}
+
+
 }
